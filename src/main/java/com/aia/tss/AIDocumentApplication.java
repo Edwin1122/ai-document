0aea6c39071e16024e93d960a266a3595dad9133
Edwin <edwinluo1122@gmail.com>
2024-08-01 11:37:47 +0800

    add openai api

--- src/main/java/com/aia/tss/AIDocumentApplication.java
+++ src/main/java/com/aia/tss/AIDocumentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJpaH2Application {
+public class AIDocumentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJpaH2Application.class, args);
+		SpringApplication.run(AIDocumentApplication.class, args);
 	}
 
 }
